b7b1bba9b1743e11a1352b00f54af478f61e2046
EC2 Default User <ec2-user@ip-172-31-6-249.ap-northeast-1.compute.internal>
2021-04-14 05:09:39 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,61 @@
+class BooksController < ApplicationController
+    
+    before_action :set_book, only: [:show, :edit, :update, :destroy]
+  
+    def index
+    @books = Book.all
+    @books = @books.where(year: params[:year]) if params[:year].present?
+    @books = @books.where(month: params[:month]) if params[:month].present?
+    end 
+    
+    def show
+    end
+    
+    def new
+        @book = Book.new
+    end
+    
+    def create
+    @book = Book.new(book_params)
+    @book = Book.new(book_params)
+    if @book.save
+        flash[:notice] = "のデータを一件登録しました。"
+    redirect_to books_path
+    else
+        flash[:alert] = "登録に失敗しました。"
+    render :new
+    end
+    end
+    
+    def edit
+    end
+    
+    def update
+    @book = Book.new(book_params)
+    if @book.update(book_params)
+        flash[:notice] = "データを一件更新しました。"
+      redirect_to action: :show
+    else
+        flash[:alert] = "更新に失敗しました。"
+      render :edit
+    end
+    end
+    
+     
+    def destroy
+    @book.destroy
+    flash[:notice] = "削除しました。"
+    redirect_to books_path
+    end
+    
+    
+    private
+    
+    def set_book
+        @book = Book.find(params[:id])
+    end
+    
+    def book_params
+    params.require(:book).permit(:year, :month, :inout, :category, :amount)
+    end
+end
